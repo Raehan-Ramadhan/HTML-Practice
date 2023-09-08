7b3740792c01cf37793bfcf1c1a4f6082e5324bf
Icy Cream <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-09-08 07:59:27 +0700

    mockup-scrollin.js fixed the image pin

--- mockup-scrollin.js
+++ mockup-scrollin.js
@@ -1,24 +1,8 @@
-class pin extends Scrollbar.ScrollbarPlugin {
-	static pluginName = 'pin';
-
-	onInit() {
-		document.querySelector('.right-side').style.transform = `translateY(${scrollbar.offset.y})`
-		console.log("hello world")
-		this._mount();
-	}
-}
-
 const Option = {
-	'damping': 0.08,
-	plugins: {pin}
+	'damping': 0.08
 }
 const scrollbar = Scrollbar.init(document.querySelector('#viewport'), Option);
 
-
-
-/*
 scrollbar.addListener((status) => {
-	rightSide.style.transform = `translateY(${100})`
-	console.log(status.offset.y)
+	document.querySelector('.right-side').style.transform = `translateY(${status.offset.y}px)`
 });
-*/
