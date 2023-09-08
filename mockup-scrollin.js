8caa4b4d7d2196857c4058473e87c91850d31572
Icy Cream <63550419+Raehan-Ramadhan@users.noreply.github.com>
2023-09-08 08:42:25 +0700

    mockup-scrollin pin on scroll magic

--- mockup-scrollin.js
+++ mockup-scrollin.js
@@ -3,6 +3,14 @@ const Option = {
 }
 const scrollbar = Scrollbar.init(document.querySelector('#viewport'), Option);
 
+
+const 	gallery = document.querySelector('.gallery'),
+		pintop = gallery.getBoundingClientRect().top,
+		pinbottom = gallery.getBoundingClientRect().bottom - pintop - window.innerHeight;
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
 scrollbar.addListener((status) => {
-	document.querySelector('.right-side').style.transform = `translateY(${status.offset.y}px)`
+	const offset = clamp(status.offset.y-pintop, 0, pinbottom);
+	document.querySelector('.right-side').style.transform = `translateY(${offset}px)`
 });
